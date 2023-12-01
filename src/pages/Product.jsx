da3d85f45bd9e8d3091b028b17c31b68aedbdb03
usamanasirgithub <qazi999subhan@gmail.com>
2023-12-01 16:18:58 +0500

    product page update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,9 +3,9 @@ import { ShopContext } from '../context/ShopContext'
 import { useParams } from 'react-router-dom';
 
 const Product = () => {
-  const {all_product} = useContext(ShopContext);
-  const {productId} = useParams();
-  const product = all_product.find((e)=>)
+  // const {all_product} = useContext(ShopContext);
+  // const {productId} = useParams();
+  // const product = all_product.find((e)=>)
 
   return (
     <div></div>
